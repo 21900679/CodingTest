00a260d0b487a6dbba48c02112392bef16b7f37e
21900679 <grapes612@naver.com>
2023-07-13 00:44:42 +0900

    수정중

--- 2580번 - 스토쿠.cpp
+++ 2580번 - 스토쿠.cpp
@@ -7,66 +7,41 @@ using namespace std;
 int input[MAX][MAX];
 int blank[MAX*MAX][2];
 int n = 0;
-bool check[9][9] = {true};
+bool check[9] = {true};
 
-// void width(int x, int y){
-//     for(int i = 0; i < MAX; i++){
-//         if(input[x][i] != 0)
-//             check[input[x][i]-1] = false;
-//     }
-// }
-
-// void height(int x, int y){
-//     for(int i = 0; i < MAX; i++){
-//         if(input[i][y] != 0)
-//             check[input[i][y]-1] = false;
-//     }
-// }
-
-// void rect(int x, int y){
-//     for(int i = x - x % 3; i < x - x % 3 + 3; i++){
-//         for(int j = y - y % 3; j < y - y % 3 + 3; j++){
-//             if(input[i][j] != 0)
-//                 check[input[i][j]-1] = false;
-//         }
-//     }
-// }
-
-void backtracking(int k){
+int backtracking(int k){
     int x = blank[k][0];
     int y = blank[k][1];
+    int result = 0;
 
     if(k == n)
-        return;
+        return 1;
 
     for(int i = 0; i < MAX; i++){
-        check[k][input[x][i]-1] = false;       // 가로
-        check[k][input[i][y]-1] = false;       // 세로
+        if(input[x][i] != 0)
+            check[input[x][i]-1] = false;       // 가로
+        if(input[i][y] != 0)
+            check[input[i][y]-1] = false;       // 세로
     }
 
     for(int i = x - x % 3; i < x - x % 3 + 3; i++){     // 사각형
         for(int j = y - y % 3; j < y - y % 3 + 3; j++){
-            check[k][input[i][j]-1] = false;
+            if(input[i][j] != 0)
+                check[input[i][j]-1] = false;
         }
     }
 
     for(int i = 0; i < MAX; i++){
-        if(check[k][i]){
+        if(check[i]){
             input[x][y] = i + 1;
-            check[k][i] = false; 
-            backtracking(k+1);
+            // for(int i = 0; i < MAX; i++)
+            //     check[i] = true; 
+            result = backtracking(k+1);
+            if(result == 1) break;
+            input[x][y] = 0;
         }
     }
-
-    // if(count(check, check + MAX, true) == 1){
-    //     for(int i = 0; i < MAX; i++){
-    //         if(check[i])
-    //             input[x][y] = i + 1;
-    //     }
-    // }
-    // for(int i = 0; i < MAX; i++)
-    //     check[i] = true;
-    // backtracking(k+1);
+    return result;
 }
 
 int main(){
