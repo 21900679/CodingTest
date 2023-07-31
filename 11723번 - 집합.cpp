4eb36271f39cce17eea7bbef924bfa8e8f4e4df0
21900679 <grapes612@naver.com>
2023-08-01 02:58:11 +0900

    시간초과

--- 11723번 - 집합.cpp
+++ 11723번 - 집합.cpp
@@ -1,24 +1,15 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
-vector<int> v;
-void Sadd(int num);
-void Sremove(int num);
-void Stoggle(int num);
-void Sall();
-void Scheck(int num);
-int output[3000000];
-int i = 0;
+bool check[20] = {false};
+vector<int> output;
 
 int main(){
 
-    
     string abc;
     int num;
-    
 
     int M;
     
@@ -26,55 +17,37 @@ int main(){
 
     for(int i = 0; i < M; i++){
         cin >> abc >> num;
-        if(abc.compare("add") == 0)
-            Sadd(num);
-        else if(abc.compare("remove") == 0)
-            Sremove(num);
-        else if(abc.compare("empty") == 0)
-            v.clear();
-        else if(abc.compare("toggle") == 0)
-            Stoggle(num);
-        else if(abc.compare("all") == 0)
-            Sall();
-        else if(abc.compare("check") == 0)
-            Scheck(num);        
+        if(abc.compare("add") == 0){
+            if(check[num-1] == false)
+                check[num-1] = true;
+        }
+        else if(abc.compare("remove") == 0){
+            if(check[num-1])
+                check[num-1] = false;
+        }
+        else if(abc.compare("empty") == 0){
+            fill_n(check, 20, false);
+        }
+        else if(abc.compare("toggle") == 0){
+            if(check[num-1])
+                check[num-1] = false;
+            else
+                check[num-1] = true;
+        }
+        else if(abc.compare("all") == 0){
+            fill_n(check, 20, true);
+        }
+        else if(abc.compare("check") == 0){
+            if(check[num-1])
+                output.push_back(1);
+            else   
+                output.push_back(0);
+        }        
     }
 
-    for(int j = 0; j < i; j++)
-        cout << output[j] << endl;
-        
+    for(int i = 0; i < output.size(); i++){
+        cout << output[i] << endl;
+    }
+
     return 0;
-}
-
-void Sadd(int num){
-    if(find(v.begin(), v.end(), num) == v.end()){   // 존재하지 않음
-        v.push_back(num);
-    }
-}
-
-void Sremove(int num){
-    if(find(v.begin(), v.end(), num) != v.end()){       // 존재함
-        v.erase(remove(v.begin(), v.end(), num), v.end());
-    }
-}
-
-void Stoggle(int num){
-    if(find(v.begin(), v.end(), num) == v.end())
-        v.erase(remove(v.begin(), v.end(), num), v.end());
-    else
-        v.push_back(num);
-}
-
-void Sall(){
-    v.clear();
-    for(int i = 1; i <= 20; i++)
-        v.push_back(i);
-}
-
-void Scheck(int num){
-    if(find(v.begin(), v.end(), num) != v.end())
-        output[i] = 1;
-    else
-        output[i] = 0;
-    i++;
 }
