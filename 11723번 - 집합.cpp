44d5fb9b53580b2b4e800e0a39fd0b1bbfd1f364
21900679 <grapes612@naver.com>
2023-08-08 18:47:41 +0900

    성공

--- 11723번 - 집합.cpp
+++ 11723번 - 집합.cpp
@@ -1,45 +1,50 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
-bool check[20] = {false};
-vector<int> output;
-
 int main(){
-
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
     string abc;
     int num;
+    long long arr = 1;
 
     int M;
     
     cin >> M;
 
-    for(int i = 0; i < M; i++){
-        cin >> abc >> num;
+    while(M--){     // 0 = false, 1 = true
+        cin >> abc;
         if(abc.compare("add") == 0){
-            check[num-1] = true;
+            cin >> num;
+            arr |= (1<<num);
         }
         else if(abc.compare("remove") == 0){
-            check[num-1] = false;
+            cin >> num;
+            arr &= ~(1<<num);
         }
         else if(abc.compare("empty") == 0){
-            fill_n(check, 20, false);
+            arr = 0;
         }
         else if(abc.compare("toggle") == 0){
-            check[num-1] = !check[num-1];
+            cin >> num;
+            if((1<<num) & arr){     // 있으면
+                arr &= ~(1<<num);
+            }
+            else        // 없으면
+                arr |= (1<<num);
         }
         else if(abc.compare("all") == 0){
-            fill_n(check, 20, true);
+            arr |= (1<<21) - 1;
         }
         else if(abc.compare("check") == 0){
-                output.push_back(check[num-1]);
-        }        
+            cin >> num;
+            if((1<<num) & arr)
+                cout << "1\n";
+            else
+                cout << "0\n";
+        }  
     }
-
-    for(int i = 0; i < output.size(); i++){
-        cout << output[i] << endl;
-    }
-
     return 0;
 }
